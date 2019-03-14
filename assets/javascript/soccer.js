451bc15fc20b0577f849f096a87f16f6badc769c
Your Name <you@example.com>
2019-03-13 22:00:47 -0500

    updates

--- assets/javascript/soccer.js
+++ assets/javascript/soccer.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
 //  1. Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called `topics`.
 //    * We chose animals for our theme, but you can make a list to your own liking.
 
-var soccer = ["world cup", "ronaldinho", "messi", "cristiano ronaldo", "wayne rooney", "steven gerrard", "f.c. barcalona",
-"manchester united", "sir alex ferguson", "bicycle kick", "elastico"]
+// var soccer = ["world cup", "ronaldinho", "messi", "cristiano ronaldo", "wayne rooney", "steven gerrard", "f.c. barcalona",
+// "manchester united", "sir alex ferguson", "bicycle kick", "elastico"]
 
 // 2. Your app should take the topics in this array and create buttons in your HTML.
 //    * Try using a loop that appends a button for each string in the array.
@@ -17,14 +17,12 @@ var soccer = ["world cup", "ronaldinho", "messi", "cristiano ronaldo", "wayne ro
 
 // 3. When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
 
-
 $("button").on("click", function(){
 
-    soccer = $(this).attr("data-soccer");
+    var soccer = $(this).attr("data-soccer");
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q" + soccer + "api_key=hEzp6VLdrLsUVR0RNuEqLStvSJfuIJEL&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + soccer + "&api_key=hEzp6VLdrLsUVR0RNuEqLStvSJfuIJEL&limit=10";
 
-})
 
 
 
@@ -33,26 +31,30 @@ $.ajax({
     method: "GET"
 })
     .then(function(response) {
+        console.log(queryURL);
+
+        console.log(response);
         
         var results = response.data;
 
         for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div>");
 
-            var rating = results[i].rating;
+            var soccerDiv = $("<div>");
 
-            var p = $("<p>").text("Rating: " + rating);
+            var p = $("<p>").text("Rating: " + results[i].rating);
 
             var soccerImage = $("<img>");
+
             soccerImage.attr("src", results[i].images.fixed_height.url);
 
-            gifDiv.prepend(p);
-            givDiv.prepend(soccerImage);
+            soccerDiv.append(p);
+            soccerDiv.append(soccerImage);
 
-            $("#soccer-gifs-appear-here").prepend(gifDiv);
-        };
+            $("#soccer-gifs-appear-here").prepend(soccerDiv);
+        }
     });
-
+})
+});
 
 
 // 4. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
@@ -67,4 +69,4 @@ $.ajax({
 
 // 8. **Rejoice**! You just made something really cool
 
-});
+
